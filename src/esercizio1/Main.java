538db1dcc77c2c64f547741ef3acc1feeff60e2c
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-07-25 11:01:10 +0200

    modifiche esercizio 1

--- src/esercizio1/Main.java
+++ src/esercizio1/Main.java
@@ -2,7 +2,13 @@ package esercizio1;
 
 public class Main {
     public static void main(String[] args) {
-        Rettangolo.stampaRettangolo();
-        Rettangolo.stampaDueRettangoli();
+        Rettangolo rettangolo1 = new Rettangolo(2, 5);
+        System.out.println("La larghezza del primo rettangolo è: " + rettangolo1.getLarghezza());
+        System.out.println("L'altezza del primo rettangolo è: " + rettangolo1.getAltezza());
+        Rettangolo rettangolo2 = new Rettangolo(7, 3);
+        System.out.println("La larghezza del secondo rettangolo è: " + rettangolo2.getLarghezza());
+        System.out.println("L'altezza del secondo rettangolo è: " + rettangolo2.getAltezza());
+
+        Rettangolo.stampaRettangolo(rettangolo1);
     }
 }
--- src/esercizio1/Rettangolo.java
+++ src/esercizio1/Rettangolo.java
@@ -9,32 +9,30 @@ public class Rettangolo {
         this.larghezza = l;
     }
 
-    public static void stampaRettangolo() {
-        Rettangolo rettangolo1 = new Rettangolo(5, 2);
-        double area = rettangolo1.altezza * rettangolo1.larghezza;
-        System.out.println("L'area del rettangolo è: " + area);
-
-        double perimetro = rettangolo1.altezza * 2 + rettangolo1.larghezza * 2;
+    public static void stampaRettangolo(Rettangolo ret1) {
+        double perimetro = altezza + larghezza;
         System.out.println("Il perimetro del rettangolo è: " + perimetro);
 
     }
 
     public static void stampaDueRettangoli() {
-        Rettangolo rettangolo1 = new Rettangolo(5, 2);
-        double area1 = rettangolo1.altezza * rettangolo1.larghezza;
-        System.out.println("L'area del primo rettangolo è: " + area1);
-        Rettangolo rettangolo2 = new Rettangolo(3, 4);
-        double area2 = rettangolo2.altezza * rettangolo2.larghezza;
-        System.out.println("L'area del secondo rettangolo è: " + area2);
 
-        System.out.println("La somma delle aree dei due rettangoli è data da: " + (area1 + area2));
 
-        double perimetro1 = rettangolo1.altezza * 2 + rettangolo1.larghezza * 2;
-        System.out.println("Il perimetro del primo rettangolo è: " + perimetro1);
-        double perimetro2 = rettangolo2.altezza * 2 + rettangolo2.larghezza * 2;
-        System.out.println("Il perimetro del secondo rettangolo è: " + perimetro2);
+    }
 
-        System.out.println("La somma dei perimetri dei due rettangoli è data da: " + (perimetro1 + perimetro2));
+    public double getAltezza() {
+        return altezza;
+    }
 
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
+    public double getLarghezza() {
+        return larghezza;
+    }
+
+    public void setLarghezza(double larghezza) {
+        this.larghezza = larghezza;
     }
 }
